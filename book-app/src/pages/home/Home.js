8d6c0044451d99aab4a0fcf1a735ee30da2e3d4f
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-30 15:02:12 +0300

    fetch data

--- book-app/src/pages/home/Home.js
+++ book-app/src/pages/home/Home.js
@@ -1,9 +1,16 @@
+import {useFetch} from '../../hooks/useFetch'
+
 import './Home.css'
 
 export default function Home(){
+    const {data, isPending, error} = useFetch('http://localhost:3000/books')
     return (
-        <div>
-            Home
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {data && data.map(book => (
+                <h2 key={book.id}>{book.title}</h2>
+            ))}
         </div>
     )
 }
