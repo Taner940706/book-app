cdaceede4f2d68aa62cc385b3ad7f9e9631597ff
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-05-30 11:42:00 +0300

    add routers and pages with components nd .css files

--- book-app/src/App.js
+++ book-app/src/App.js
@@ -1,9 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/home/Home.js'
+import Book from './pages/book/Book.js'
+import Search from './pages/search/Search.js'
+import Create from './pages/create/Create.js'
+
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/:id" element={<Book />} />
+        <Route path="/search"  element={<Search />}/>
+        <Route path="/create" element={<Create />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ book-app/src/pages/book/Book.js
@@ -0,0 +1,9 @@
+import './Book.css'
+
+export default function Book(){
+    return(
+        <div>
+            Book
+        </div>
+    )
+}
