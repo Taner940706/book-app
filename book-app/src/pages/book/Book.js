88353b90cfa9a781860ae16c509237581c6d06dd
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-02 15:28:07 +0300

    fetch book

--- book-app/src/pages/book/Book.js
+++ book-app/src/pages/book/Book.js
@@ -1,9 +1,17 @@
+import {useParams} from 'react-router-dom'
+import {useFetch} from '../../hooks/useFetch'
+
 import './Book.css'
 
 export default function Book(){
+    const {id} = useParams()
+    const url = 'http://localhost:3000/books/' + id
+    const {error, isPending, data: book} = useFetch(url)
     return(
-        <div>
-            Book
+        <div className='book'>
+            {error && <p className='error'></p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {book && <h1>{book.title}</h1>}
         </div>
     )
 }
