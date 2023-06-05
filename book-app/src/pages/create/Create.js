dbd54eb3297433a3a9d058b9f2e178fc5a617c8e
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-05 11:01:28 +0300

    add multiple genres functionality

--- book-app/src/pages/create/Create.js
+++ book-app/src/pages/create/Create.js
@@ -1,11 +1,28 @@
-import {useState} from 'react'
+import {useRef, useState} from 'react'
 import './Create.css'
 
 export default function Create(){
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
     const [genres, setGenres] = useState('')
-    const [topic, setTopic] = useState('')
+    const [newGenre, setNewGenre] = useState('')
+    const genreInput = useRef(null)
+    const [topic, setTopic] = useState([])
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        const x = newGenre.trim()
+
+        if (x && !genres.includes(x)){
+            setGenres(prevGenre => [...prevGenre, x])
+        }
+        setNewGenre('')
+        genreInput.current.focus()
+    }
 
     return (
         <div className='create'>
@@ -14,19 +31,27 @@ export default function Create(){
             <form onSubmit={handleSubmit}>
                 <label>
                     <span>Title:</span>
-                    <input type="text" onChange={(e) => setTitle(e.target.value)} value="title" required></input>
+                    <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} required></input>
                 </label>
                 <label>
                     <span>Author:</span>
-                    <input type="text" onChange={(e) => setTitle(e.target.value)} value="author" required></input>
+                    <input type="text" onChange={(e) => setAuthor(e.target.value)} value={author} required></input>
                 </label>
                 <label>
                     <span>Genres:</span>
-                    <input type="text" onChange={(e) => setTitle(e.target.value)} value="genres" required></input>
+                    <div className='genres'>
+                        <input
+                        type="text"
+                        onChange={(e) => setNewGenre(e.target.value)}
+                        value={newGenre}
+                        ref={genreInput}
+                        />
+                    </div>
                 </label>
+                <p>Current genres are: {genres.map(i => <em>{i}, </em>)}</p>
                 <label>
                     <span>Topic:</span>
-                    <textarea onChange={(e) => setTitle(e.target.value)} value="topic" required></textarea>
+                    <textarea onChange={(e) => setTopic(e.target.value)} value={topic} required></textarea>
                 </label>
                 <button className='btn'>Send</button>
             </form>
