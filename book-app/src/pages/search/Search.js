13b0eaa9a28ff383a71574b55324b9e06eab2193
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-06-08 15:33:17 +0300

    search result

--- book-app/src/pages/search/Search.js
+++ book-app/src/pages/search/Search.js
@@ -1,9 +1,24 @@
+import { useLocation } from 'react-router-dom'
+import {useFetch} from '../../hooks/useFetch'
+import BookList from '../../components/BookList'
+
 import './Search.css'
 
 export default function Search(){
+
+    const queryString = useLocation().search
+    const queryParams =  new URLSearchParams(queryString)
+    const query = queryParams.get('q')
+
+    const url = 'http://localhost:3000/books?q=' + query
+    const {error, isPending, data} = useFetch(url)
+
     return (
         <div>
-            Search
+            <h2 className='page-title'>Book result with "{query}"</h2>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {data && <BookList books={data} />}
         </div>
     )
 }
